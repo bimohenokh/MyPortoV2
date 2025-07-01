9867a415eae9c660315fc4998ee294a5c847627a
bimohenokh <bimo.barata2@gmail.com>
2025-07-01 19:39:11 +0700

    update Footer element

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,60 +1,66 @@
-import { Container } from '@/components/Footer/styles'
+import {Container} from '@/components/Footer/styles'
 import reactIcon from '@/assets/react-icon.svg'
 import linkedin from '@/assets/linkedin.svg'
 import githubIcon from '@/assets/github.svg'
 import whatsapp from '@/assets/whatsapp.svg'
-import telegram from '@/assets/telegram.svg'
-import instagramIcon from '@/assets/instagram.svg'
+// import telegram from '@/assets/telegram.svg'
+// import instagramIcon from '@/assets/instagram.svg'
 
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
+      {/*TODO nanti tambah*/}
+      {/*<a href="https://bimohenokh.in" className="logo" >*/}
+      {/*  <span>www.bimo</span>*/}
+      {/*  <span>henokh.in</span>*/}
+      {/*</a>*/}
+      <a href="" className="logo">
+        <span>Bimo Henokh Barata</span>
       </a>
+
       <div>
         <p>
-          This Website was made with <img src={reactIcon} alt="React" />
-          {/* <span>❤️</span> */}
+          This Website was made with <img src={reactIcon} alt="React"/>
         </p>
       </div>
+
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/codevinayak"
+          href="https://www.linkedin.com/in/bimo-henokh-barata-311333235/"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={linkedin} alt="Linkedin" />
+          <img src={linkedin} alt="Linkedin"/>
         </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/bimohenokh/"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={githubIcon} alt="GitHub" />
+          <img src={githubIcon} alt="GitHub"/>
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
+          href="https://api.whatsapp.com/send/?phone=%2B6285217073494&text=Hello+Bimo+I+found+your+contact+through+portfolio+site.%0A%0A"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={whatsapp} alt="Whatsapp" />
+          <img src={whatsapp} alt="Whatsapp"/>
         </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
+        {/*TODO nanti tambahin*/}
+        {/*<a*/}
+        {/*  href=""*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noreferrer"*/}
+        {/*>*/}
+        {/*  <img src={telegram} alt="telegram"/>*/}
+        {/*</a>*/}
+        {/*<a*/}
+        {/*  href=""*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noreferrer"*/}
+        {/*>*/}
+        {/*  <img src={instagramIcon} alt="Instagram"/>*/}
+        {/*</a>*/}
       </div>
     </Container>
   )
